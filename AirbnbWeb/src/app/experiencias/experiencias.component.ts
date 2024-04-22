dd65dffdfb77e778ff5947229419bfbcf9ce85ae
RobFillingham <robertfillingham@hotmail.com>
2024-04-21 21:57:58 -0600

    experiences

--- AirbnbWeb/src/app/experiencias/experiencias.component.ts
+++ AirbnbWeb/src/app/experiencias/experiencias.component.ts
@@ -2,19 +2,24 @@ import { Component } from '@angular/core';
 import { ExperienciasService } from '../services/places.service';
 import { RouterModule } from '@angular/router';
 import { Experiencias } from '../interfaces/experiencias';
+import { CommonModule } from '@angular/common';
+import { DarkBackService } from '../services/back/dark-back.service';
 
 @Component({
   selector: 'app-experiencias',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experiencias.component.html',
   styleUrl: './experiencias.component.css'
 })
 export class ExperienciasComponent {
 
   array:Experiencias[]=[];
+  dark : boolean = false;
+  background : string = "white";
+  color : string = "black";
 
-  constructor(public experienciasService: ExperienciasService){
+  constructor(public experienciasService: ExperienciasService, public darkBackService: DarkBackService){
     //Primero se ejecuta el constructor, luego el ngOnInit
   }
 
@@ -27,6 +32,16 @@ ngOnInit(){
        next: this.successRequest.bind(this),
        error:(err)=>{console.log(err)}
      });
+     this.darkBackService.dark$.subscribe(dark => {
+      this.dark = dark;
+      if(this.dark){
+        this.background = "black";
+        this.color = "white";
+      }else{
+        this.background = "white";
+        this.color = "black";
+      }
+    });
    }
 
    successRequest(data:any):void{
