cf73431437a137cb4f5c69afa9c589c0c9f4dd03
RobFillingham <robertfillingham@hotmail.com>
2024-06-15 13:26:08 -0600

    Firebase Setup

--- AirbnbWeb/src/app/app.config.ts
+++ AirbnbWeb/src/app/app.config.ts
@@ -4,7 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"airbnbfinal-fe212","appId":"1:1007996376875:web:263dbbc57209a4f262d76e","storageBucket":"airbnbfinal-fe212.appspot.com","apiKey":"AIzaSyCGYaYAclr6hcLM8zbqZNVJoktuKNgnigU","authDomain":"airbnbfinal-fe212.firebaseapp.com","messagingSenderId":"1007996376875"})), provideAuth(() => getAuth())]
 };
